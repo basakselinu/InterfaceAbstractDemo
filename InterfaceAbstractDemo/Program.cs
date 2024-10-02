b1451a94fb735452214769b906b146497d36532d
basak <basak@BASAKSELIN>
2024-10-02 18:38:11 +0300

    KodlamaIO - Ödev

--- InterfaceAbstractDemo/Program.cs
+++ InterfaceAbstractDemo/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer {
+                Id = 1,
+                DateOfBirth=new DateTime(2000,6,4),
+                FirstName="Başak Selin",
+                LastName="Ünsal",
+                NationalityId="888888"
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
